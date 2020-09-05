e4be6b1e8de02c9872dd4f65cdcfb332228dd6b9
Steven Kaan <steven.kaan@sogeti.com>
2020-09-05 03:09:46 +0200

    Made new plugin which will log all players names

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# Example
-An example greeter plugin
+# Chambers of Xeric Name Plugin
+This plugin will put all name in a textare when a raid starts with 2 or more people.
---
+++ src/test/java/wtf/kaan/raidplayernames/RaidPlayerNamesPluginTest.java
@@ -0,0 +1,12 @@
+package wtf.kaan.raidplayernames;
+
+import net.runelite.client.RuneLite;
+import net.runelite.client.externalplugins.ExternalPluginManager;
+
+public class RaidPlayerNamesPluginTest {
+    public static void main(String[] args) throws Exception
+    {
+        ExternalPluginManager.loadBuiltin(RaidPlayerNamesPlugin.class);
+        RuneLite.main(args);
+    }
+}

c5b18b7e92acf2cf11d145b68ee1b4370e4d81d4
WannaKnow <stevenkaan@gmail.com>
2021-07-05 17:09:21 +0200

    Removed test code

--- src/main/java/dev/wannaknow/raidplayernames/RaidPlayerNamesPanel.java
+++ src/main/java/dev/wannaknow/raidplayernames/RaidPlayerNamesPanel.java
@@ -1,18 +1,11 @@
 package dev.wannaknow.raidplayernames;
 
-import lombok.extern.slf4j.Slf4j;
 import net.runelite.client.ui.PluginPanel;
 
 import java.awt.*;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
-
-@Slf4j
 public class RaidPlayerNamesPanel extends PluginPanel {
-
-    private List<RaidPlayerNamesBox> boxes = new ArrayList<>();
 
     RaidPlayerNamesPanel(RaidPlayerNamesPlugin plugin) {
         getParent().setLayout(new BorderLayout());
